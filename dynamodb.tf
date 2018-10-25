6aac0dc2685a57c53aad9f3ef99ed265968e866c
Jan Ritter <jan-ritter@outlook.com>
2018-10-25 22:26:10 +0200

    feat: added dynamodb table for repositories

--- dynamodb.tf
+++ dynamodb.tf
@@ -16,3 +16,22 @@ resource "aws_dynamodb_table" "tower-configuration" {
     Environment = "production"
   }
 }
+
+resource "aws_dynamodb_table" "repositories" {
+  name           = "auto-staging-tower-repositories"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "repository"
+
+  attribute {
+    name = "repository"
+    type = "S"
+  }
+
+  # Remaining attributes must be created in the application
+
+  tags {
+    Name        = "auto-staging-tower-repositories"
+    Environment = "production"
+  }
+}
