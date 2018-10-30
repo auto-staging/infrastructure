ed0c955eb2db4531ff5087801a547dbd63691e94
Jan Ritter <jan-ritter@outlook.com>
2018-10-30 21:54:22 +0100

    refactor: removed dynamodb table for tower config after moving config into lambda env vars

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,22 +1,3 @@
-resource "aws_dynamodb_table" "tower-configuration" {
-  name           = "auto-staging-tower-conf"
-  read_capacity  = 5
-  write_capacity = 5
-  hash_key       = "towerStage"
-
-  attribute {
-    name = "towerStage"
-    type = "S"
-  }
-
-  # Remaining attributes must be created in the application
-
-  tags {
-    Name        = "auto-staging-tower-conf"
-    Environment = "production"
-  }
-}
-
 resource "aws_dynamodb_table" "repositories" {
   name           = "auto-staging-tower-repositories"
   read_capacity  = 5
