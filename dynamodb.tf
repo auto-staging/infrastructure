02059cc450779105539943e2ffc54db1a6f6b159
Jan Ritter <jan-ritter@outlook.com>
2018-11-09 14:15:54 +0100

    feat: added dynamodb table for global config

--- dynamodb.tf
+++ dynamodb.tf
@@ -41,3 +41,22 @@ resource "aws_dynamodb_table" "environments" {
     Environment = "production"
   }
 }
+
+resource "aws_dynamodb_table" "global_config" {
+  name           = "auto-staging-repositories-global-config"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "stage"
+
+  attribute {
+    name = "stage"
+    type = "S"
+  }
+
+  # Remaining attributes must be created in the application
+
+  tags {
+    Name        = "auto-staging-repositories-global-config"
+    Environment = "production"
+  }
+}
